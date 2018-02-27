28c0d8e4a1516f8023d6ebe677cdd7cfdecb7c26
alex-khrust <st.alex.ser@gmail.com>
2018-02-27 16:49:13 +0200

    убрал base и скачал все файлы с сервера

---
+++ js/site.js
@@ -0,0 +1,29 @@
+var isLoading = false;
+
+$(document).ready(function () {
+        $(".fancybox").fancybox();
+    });
+jQuery ( "a.fancybox" ). fancybox ({ 
+    "onComplete" : function ()  { 
+        clearTimeout ( jQuery . fancybox . slider ); 
+        jQuery . fancybox . slider = setTimeout ( "jQuery.fancybox.next()" , 5000 ); 
+    } 
+});
+	
+function getUrlVars() {
+    return window.location.href.slice(window.location.href.indexOf('?')).split(/[&?]{1}[\w\d]+=/);
+}
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
+function Loading() {
+    
+    if (!isLoading) {
+        $.LoadingOverlay("show");
+        isLoading = true;
+    } else {
+        $.LoadingOverlay("hide");
+        isLoading = false;
+    }
+}
